58ff011cade9dea5fda6b43b8a155607c06ca959
Abdullah Khallouf <a.khallouf@outlook.com>
2021-08-09 17:57:23 +0300

    created add todo feature

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd1b79378f75ae1b4d4d"; }
+/******/ 		__webpack_require__.h = function() { return "c79da50be2deeac61ee3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Head from "next/head";
 import styles from "./home.module.scss";
-import {Container} from "@material-ui/core";
+import {Button, Container, TextField} from "@material-ui/core";
 import {withTranslation} from "../../i18n";
 import Todo from "../../components/Todo/Todo";
 import {Prisma, Todo as TodoType} from "@prisma/client";
@@ -20,6 +20,8 @@ export const getServerSideProps = async () => {
 
 function Home({t}) {
 	const [todos, setTodos] = useState<TodoType[]>();
+	const [title, setTitle] = useState<string>("");
+	const [description, setDescription] = useState<string>("");
 
 	const getTodos = async () => {
 		try {
@@ -31,20 +33,83 @@ function Home({t}) {
 			console.log("error while getting todos", err);
 		}
 	};
+
 	useEffect(() => {
 		getTodos();
 	}, []);
 
+	const createTodo = async (ev) => {
+		ev.preventDefault();
+		if (title == "" || description == "") {
+			alert("complete todo info frist!");
+			return;
+		}
+		const response = await fetch("/api/todos", {
+			method: "POST",
+			headers: {},
+			body: JSON.stringify({
+				title: title,
+				description: description,
+			}),
+		});
+		console.log(await response.json());
+	};
+
+	const titleChangeHandler = (ev: any) => {
+		setTitle(ev.target.value);
+		console.log(title);
+	};
+	const descriptionChangeHandler = (ev: any) => {
+		setDescription(ev.target.value);
+	};
 	return (
 		<Container style={{height: "100%"}}>
 			<Head>
 				<title>{t("home:title")} </title>
 			</Head>
 
+			<form
+				style={{
+					width: "50%",
+					height: "50%",
+					marginTop: 100,
+				}}
+				onSubmit={createTodo}
+				noValidate
+				autoComplete="off"
+			>
+				<TextField
+					id="title"
+					label="title"
+					fullWidth
+					variant="filled"
+					value={title}
+					onChange={titleChangeHandler}
+				/>
+				<br />
+				<TextField
+					style={{marginTop: 10}}
+					id="description"
+					label="description"
+					fullWidth
+					variant="filled"
+					value={description}
+					onChange={descriptionChangeHandler}
+				/>
+
+				<Button type="submit" style={{marginTop: 10}}>
+					add Todo
+				</Button>
+			</form>
+
 			{todos &&
 				todos.map((todo: TodoType) => {
 					return (
-						<Todo titleProp={todo.title} descriptionProp={todo.description} />
+						<Todo
+							key={todo.id}
+							titleProp={todo.title}
+							descriptionProp={todo.description}
+						/>
 					);
 				})}
 
